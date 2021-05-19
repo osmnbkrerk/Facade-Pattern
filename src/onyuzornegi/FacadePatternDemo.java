fce1620f00057b52978c993ab3ab4b2a055b7fe3
osmnbkrerk <84410718+osmnbkrerk@users.noreply.github.com>
2021-05-19 16:15:28 +0300

    Create FacadePatternDemo.java

---
+++ src/onyuzornegi/FacadePatternDemo.java
@@ -0,0 +1,11 @@
+package onyuzornegi;
+public class FacadePatternDemo {
+
+    public static void main(String[] args) {
+        ShapeMaker shapeMaker = new ShapeMaker();
+        shapeMaker.drawCircle();
+        shapeMaker.drawRectangle();
+        shapeMaker.drawSquare();
+        
+    }    
+}
